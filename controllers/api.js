fbb37396448ab753bf465b40e9024c79c08df2b9
Rees Dart <thedominicparker@gmail.com>
2020-07-14 09:26:01 -0700

    correcting capitalization of filepath

--- controllers/api.js
+++ controllers/api.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const newDB = require("../db/DB");
+const newDB = require("../db/db");
 const { uuid } = require('uuidv4');
 
 
